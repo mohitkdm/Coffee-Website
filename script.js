3b4bfaab1b8bbea5b772937fc31fb929e1d35bf6
Mohit <mohitkdm21@gmail.com>
2025-01-04 09:56:40 +0530

    coffee website

---
+++ script.js
@@ -0,0 +1,52 @@
+// alert("Hello KDM Coffee")
+
+const navLinks=document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton=document.querySelector("#menu-open-button");
+const menuCloseButton=document.querySelector("#menu-close-button"); 
+
+// Toggle mobile menu visibility
+menuOpenButton.addEventListener("click",()=>{
+    document.body.classList.toggle("show-mobile-menu");
+})
+
+// close the menu in the mobile view
+menuCloseButton.addEventListener("click",()=> menuOpenButton.click())
+
+// close the menu when nav link clicked
+navLinks.forEach(link =>{
+    link.addEventListener("click",()=>menuOpenButton.click())
+})
+
+// Initialize Swiper 
+const swiper = new Swiper('.slider-wrapper', {
+    loop: true,
+    grabCursor:true,
+    spaceBetween:25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      dynamicBullets:true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // Responsive breakpoints
+    breakpoints:{
+        0:{
+            slidesPerView:1
+        },
+        768:{
+            slidesPerView:2
+        },
+        1024:{
+            slidesPerView:3
+        },
+    }
+  });
+  
